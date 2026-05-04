1dc612df7818690d364394d0b1976c0e278846e1
KRAKENN8 <krakeen99@gmail.com>
2026-05-04 10:50:37 +0300

    New gradle settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,9 @@ android {
         versionCode = 1
         versionName = "1.0"
 
+        val apiKey = properties["API_KEY"].toString()
+        buildConfigField("String", "API_KEY", "\"$apiKey\"")
+
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -47,7 +50,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
-    
+
     implementation(libs.retrofit)
     implementation(libs.retrofit.gson)
     implementation(libs.gson)
